df873d240871a448aef87d2ff0db99888d6f5e45
Denzel YourSurname <denzelmushayawedu@gmail.com>
2025-07-10 13:02:49 +0100

    Summer Project

--- app.py
+++ app.py
@@ -1,23 +1,71 @@
-#create route to serve the index.html file
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import os
+from werkzeug.utils import secure_filename
 
-app = Flask(__name__)   
+app = Flask(__name__)
+
+UPLOAD_FOLDER = 'static/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Ensure upload folder exists
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+@app.route('/timetable', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        files = request.files.getlist('photos')
+        for file in files:
+            if file.filename != '':
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect(url_for('upload_file'))
+
+    # List uploaded images
+    images = os.listdir(UPLOAD_FOLDER)
+    return render_template('timetable.html', images=images)
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/loginchoice', methods=['GET', 'POST'])
+def login_choice():
+    if request.method == 'POST':
+        user_type = request.form.get('user_type')
+        return redirect(url_for('login', user_type=user_type))
+    return render_template('loginchoice.html')
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    user_type = request.args.get('user_type', 'user')  # default to 'user'
+    if user_type == 'student':
+        title = "Student Login"
+    elif user_type == 'coach':
+        title = "Coach Login"
+    else:
+        title = "User Login"
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        return render_template('timetable.html', username=username)
+    return render_template('login.html', title=title)
 
 @app.route('/register')
 def register():
     return render_template('register.html')
+'''
+@app.route('/timetable', methods=['GET', 'POST'])
+def timetable():
+    if request.method == 'POST':
+        files = request.files.getlist('photos')
+        for file in files:
+            if file.filename != '':
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect(url_for('timetable'))
 
-@app.route('/time-table')
-def time_table():
-    return render_template('timetable.html')
-
+    images = os.listdir(app.config['UPLOAD_FOLDER'])
+    return render_template('timetable.html', images=images)'''
 
 @app.route('/timetable-couch')
 def time_table_couch():
@@ -26,7 +74,6 @@ def time_table_couch():
 @app.route('/timetable-grade')
 def time_table_grade():
     return render_template('timetable-grade.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
